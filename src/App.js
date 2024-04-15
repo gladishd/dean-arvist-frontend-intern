2bfb1cd53ae27f1cf54b2fc6c3841aeeb52bee50
gladishd <gladish.dean@gmail.com>
2024-04-15 15:37:27 -0500

    Commits right before adding the useEffect hook which is triggered "every time" the todos array changes. The purpose of the useEffect hook is to set the data on localStorage.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
+  const [task, setTask] = useState('');
+  const handleAddTodo = () => {
+    if (task !== "") {
+      setTodos([...todos, task]);
+      setTask('');
+    }
+  };
+  const handleInputChange = (event) => {
+    console.log("The input value is ", event.target.value)
+    setTask(event.target.value);
+  };
+  const handleDeleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <input
+          type="text"
+          value={task}
+          onChange={handleInputChange}
+          placeholder="Add a new task"
+        />
+        <button onClick={handleAddTodo}>Add Todo</button>
+        <ul>
+          {
+            console.log("The current todos are ", todos)
+          }
+          {
+            todos.map((todo, index) => (
+              <li key={index}>
+                {todo}
+                <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+              </li>
+            ))
+          }
+        </ul>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Todo List
+          For <code>Arvist</code> Food Management Company.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.arvist.ai/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Arvist Website
         </a>
       </header>
     </div>
